5ecef79191a9e3230b86befda5bd50a05b794b9e
Jason Hall <jasonbrianhall@gmail.com>
2025-02-10 15:07:44 -0500

    High scores

--- compile.sh
+++ compile.sh
@@ -3,7 +3,7 @@
 # Check and compile Linux version if g++ is available
 if command -v g++ &> /dev/null; then
     echo "Compiling Linux version..."
-    g++ minesweeper.cpp -lncurses -o minesweeper
+    g++ minesweeper.cpp highscores.cpp -lncurses -o minesweeper
 else
     echo "g++ not found - skipping Linux build"
 fi
