85fc69eb3920141fdbbdf4a0767deb5f33a52419
Jason Hall <jasonbrianhall@gmail.com>
2025-02-10 10:49:40 -0500

    Minesweeper compile

---
+++ compile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Check and compile Linux version if g++ is available
+if command -v g++ &> /dev/null; then
+    echo "Compiling Linux version..."
+    g++ minesweeper -lncurses -o minesweeper
+else
+    echo "g++ not found - skipping Linux build"
+fi
