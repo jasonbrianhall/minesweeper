bc6194804a06ae525676f4c3f971f97f348780c7
Jason Hall <jasonbrianhall@gmail.com>
2025-02-10 19:37:09 -0500

    Latest

--- win_minesweeper.h
+++ win_minesweeper.h
@@ -1,13 +1,10 @@
 #pragma once
 
-#include <Windows.h>
 #include <string>
+#include <vector>
 #include "highscores.h"
 
-using namespace System;
-using namespace System::Windows::Forms;
-using namespace System::Drawing;
-
+// Native C++ classes
 enum class Difficulty {
     EASY,
     MEDIUM,
@@ -23,7 +20,6 @@ public:
 
 class Minesweeper {
 public:
-    Minesweeper();
     bool firstMove;
     bool gameOver;
     bool won;
@@ -36,6 +32,7 @@ public:
     std::vector<std::vector<bool>> revealed;
     std::vector<std::vector<bool>> flagged;
 
+    Minesweeper();
     void setDifficulty(Difficulty diff);
     void reset();
     void initializeMinefield(int row, int col);
@@ -47,72 +44,8 @@ public:
     void saveHighscore();
 };
 
+// Forward declarations for managed classes
 namespace MinesweeperGame {
-    public ref class MinesweeperWrapper {
-    private:
-        Minesweeper* nativeMinesweeper;
-
-    public:
-        MinesweeperWrapper();
-        ~MinesweeperWrapper();
-
-        property Minesweeper* NativeMinesweeper {
-            Minesweeper* get();
-        }
-
-        void SetDifficulty(int difficulty);
-        bool IsHighScore(int time);
-        void SaveHighScore(String^ name);
-        System::Collections::Generic::List<String^>^ GetHighScores();
-        void RevealCell(int row, int col);
-        void ToggleFlag(int row, int col);
-        bool IsRevealed(int row, int col);
-        bool IsFlagged(int row, int col);
-        bool IsMine(int row, int col);
-        int GetAdjacentMines(int row, int col);
-        bool IsGameOver();
-        bool HasWon();
-        String^ GetTime();
-        void Reset();
-        int GetWidth();
-        int GetHeight();
-    };
-
-    public ref class MainForm : public System::Windows::Forms::Form {
-    private:
-        MinesweeperWrapper^ minesweeper;
-        array<Button^, 2>^ grid;
-        MenuStrip^ menuStrip;
-        ToolStrip^ toolStrip;
-        StatusStrip^ statusStrip;
-        ToolStripStatusLabel^ statusLabel;
-        ToolStripStatusLabel^ timeLabel;
-        TextBox^ instructionsBox;
-        System::Drawing::Font^ buttonFont;
-        System::Windows::Forms::Timer^ gameTimer;
-        Form^ highScoreForm;
-        TextBox^ nameEntryBox;
-        ListView^ highScoreList;
-
-        void InitializeComponent();
-        void UpdateTimer(Object^ sender, EventArgs^ e);
-        void MainForm_KeyDown(Object^ sender, KeyEventArgs^ e);
-        void InitializeGrid();
-        void UpdateCell(int row, int col);
-        void UpdateAllCells();
-        void Cell_MouseUp(Object^ sender, MouseEventArgs^ e);
-        void ShowHighScoreEntry();
-        void SubmitHighScore(Object^ sender, EventArgs^ e);
-        void ShowHighScores();
-        void CloseHighScores(Object^ sender, EventArgs^ e);
-        void NewGame_Click(Object^ sender, EventArgs^ e);
-        void Exit_Click(Object^ sender, EventArgs^ e);
-        void SetEasy_Click(Object^ sender, EventArgs^ e);
-        void SetMedium_Click(Object^ sender, EventArgs^ e);
-        void SetHard_Click(Object^ sender, EventArgs^ e);
-        void UpdateStatus(String^ message);
-
-    public:
-        MainForm();
-    };
+    ref class MinesweeperWrapper;
+    ref class MainForm;
 }
