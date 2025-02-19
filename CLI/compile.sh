3126472eaff6c63ca0b3c55993a47a131c74bbc1
Jason Hall <jasonbrianhall@gmail.com>
2025-02-19 12:22:42 -0500

    PCURSES

--- CLI/compile.sh
+++ CLI/compile.sh
@@ -7,3 +7,16 @@ if command -v g++ &> /dev/null; then
 else
     echo "g++ not found - skipping Linux build"
 fi
+
+# Check and compile Windows version if cross-compiler is available
+if command -v x86_64-w64-mingw32-g++ &> /dev/null; then
+    mkdir win_minesweeper -p
+
+    echo "Compiling Windows version (PDCurses) ..."
+    x86_64-w64-mingw32-g++ minesweeper.cpp highscores.cpp -lpdcurses -std=c++14 -o win_minesweeper/minesweepr_pdcurses.exe
+    ./collect_dlls.sh win_minesweeper/minesweepr_pdcurses.exe /usr/x86_64-w64-mingw32/sys-root/mingw/bin win_minesweeper
+
+else
+    echo "Windows cross-compiler not found - skipping Windows build"
+fi
+
